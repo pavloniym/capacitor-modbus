bbef8373dd74f1efee6f2679050ed4be1102b5cc
NicolasRoehm <nicolas.roehm@gmail.com>
2022-12-06 14:03:41 +0100

    [Upd] try catch

--- android/src/main/java/com/frenchstack/plugins/modbus/ModbusClientPlugin.java
+++ android/src/main/java/com/frenchstack/plugins/modbus/ModbusClientPlugin.java
@@ -6,7 +6,7 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
-
+import android.util.Log;
 import org.json.JSONException;
 
 import java.io.IOException;
@@ -40,34 +40,58 @@ public class ModbusClientPlugin extends Plugin {
     }
 
     @PluginMethod
-    public void connect(PluginCall call) throws IOException
+    public void connect(PluginCall call)
     {
-        implementation.Connect();
+        try
+        {
+            implementation.Connect();
+        }
+        catch (IOException e)
+        {
+            call.reject("Unable to connect");
+            Log.e("modbus", e.getMessage());
+        }
 
         call.resolve();
     }
 
     @PluginMethod
-    public void disconnect(PluginCall call) throws IOException
+    public void disconnect(PluginCall call)
     {
-        implementation.Disconnect();
+        try
+        {
+            implementation.Disconnect();
+        }
+        catch (IOException e)
+        {
+            call.reject("Unable to disconnect");
+            Log.e("modbus", e.getMessage());
+        }
 
         call.resolve();
     }
 
     @PluginMethod
-    public void connectTo(PluginCall call) throws IOException
+    public void connectTo(PluginCall call)
     {
         String ip = call.getString("ip");
         int port = call.getInt("port");
 
-        implementation.Connect(ip, port);
+        try
+        {
+            implementation.Connect(ip, port);
+        }
+        catch (IOException e)
+        {
+            call.reject("Unable to connect");
+            Log.e("modbus", e.getMessage());
+        }
 
         call.resolve();
     }
 
     @PluginMethod
-    public void isConnected(PluginCall call) throws IOException
+    public void isConnected(PluginCall call)
     {
         boolean result = implementation.isConnected();
 
@@ -77,39 +101,56 @@ public class ModbusClientPlugin extends Plugin {
     }
 
     @PluginMethod
-    public void readHoldingRegisters(PluginCall call) throws IOException, ModbusException
+    public void readHoldingRegisters(PluginCall call)
     {
         int start = call.getInt("start");
         int count = call.getInt("count");
 
-        int[] result = implementation.ReadHoldingRegisters(start, count);
+        try {
+            int[] result = implementation.ReadHoldingRegisters(start, count);
+            JSArray jsArray = new JSArray();
+            for (int i = 0; i < result.length; i++)
+            {
+                jsArray.put(result[i]);
+            }
 
-
-        JSArray jsArray = new JSArray();
-        for (int i = 0; i < result.length; i++)
-        {
-            jsArray.put(result[i]);
+            JSObject jsResponse = new JSObject();
+            jsResponse.put("result", jsArray);
+            call.resolve(jsResponse);
         }
-
-        JSObject jsResponse = new JSObject();
-        jsResponse.put("result", jsArray);
-        call.resolve(jsResponse);
+        catch (IOException e) {
+            call.reject("IOException : Unable to readHoldingRegisters");
+            Log.e("modbus", e.getMessage());
+        }
+        catch (ModbusException e) {
+            call.reject("ModbusException : Unable to readHoldingRegisters");
+            Log.e("modbus", e.getMessage());
+        }
     }
 
     @PluginMethod
-    public void convertRegistersToFloat(PluginCall call) throws IOException, ModbusException, JSONException {
-        int order = call.getInt("order");
-        JSArray jsRegisters = call.getArray("registers");
-        int[] registers = new int[jsRegisters.length()];
-        for (int i = 0; i < jsRegisters.length(); i++) {
-            registers[i] = jsRegisters.getInt(i);
+    public void convertRegistersToFloat(PluginCall call)
+    {
+        try {
+            int order = call.getInt("order");
+            JSArray jsRegisters = call.getArray("registers");
+            int[] registers = new int[jsRegisters.length()];
+            for (int i = 0; i < jsRegisters.length(); i++) {
+                registers[i] = jsRegisters.getInt(i);
+            }
+            float result = implementation.ConvertRegistersToFloat(registers, ModbusClient.RegisterOrder.values()[order]);
+            JSObject jsResponse = new JSObject();
+            jsResponse.put("result", result);
+            call.resolve(jsResponse);
         }
-
-        float result = implementation.ConvertRegistersToFloat(registers, ModbusClient.RegisterOrder.values()[order]);
-
-        JSObject jsResponse = new JSObject();
-        jsResponse.put("result", result);
-        call.resolve(jsResponse);
+        catch (IllegalArgumentException e) {
+            call.reject("IllegalArgumentException : Unable to convertRegistersToFloat");
+            Log.e("modbus", e.getMessage());
+        }
+        catch (JSONException e) {
+            call.reject("JSONException : Unable to convertRegistersToFloat");
+            Log.e("modbus", e.getMessage());
+        }
     }
 
     
